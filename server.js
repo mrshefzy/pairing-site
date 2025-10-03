30a39a130e4d5a7c1176305dfaf75c9a312ae7e5
mrshefzy <shefzyfrosh01@gmail.com>
2025-10-03 04:44:44 +0000

    Final fix for file paths

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const os = require('os'); // <-- Import the 'os' module
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/request-code', async (req, res) => {
